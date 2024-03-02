b99afe19ea92b7017ae0c1da44aa857dcc6f367f
Arunvijay28 <arunvijaynov10@gamil.com>
2024-03-02 20:52:23 +0530

    search

---
+++ search/BinarySearch.java
@@ -0,0 +1,40 @@
+package search;
+import java.util.Scanner;
+class summa{
+    public int bin(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while (start<=end){
+            int mid=(start+end)/2;
+            if (arr[mid]<target){
+                start=mid+1;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        int target;
+        Scanner scanner=new Scanner(System.in);
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter a no:");
+            int j=scanner.nextInt();
+            arr[i]=j;
+        }
+
+        System.out.println("Enter target :");
+        target=scanner.nextInt();
+        summa s = new summa();
+        System.out.println("after binary search"+s.bin(arr,target));
+    }
+}
